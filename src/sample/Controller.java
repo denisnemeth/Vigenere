adb0c86e46340b9faf964896c03212d773b4d5a7
denisnemeth <55528169+denisnemeth@users.noreply.github.com>
2020-02-07 11:35:59 +0100

    added encryption

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -38,6 +38,37 @@ public class Controller {
         }
     }
     public void encrypt(ActionEvent actionEvent) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            if (file != null) {
+                String line, key = txtfld_key.getText().toUpperCase(), encryptedText = "";
+                char[] arrKey = key.toCharArray();
+                int pos, startValue, endValue;
+                for (pos = 0; pos <= key.length(); pos++) {
+                    while ((startValue = reader.read()) != -1) {
+                        char letter = (char) startValue;
+                        if (pos == key.length()) {
+                            pos = 0;
+                        }
+                        if (startValue < 65 || (startValue > 90 && startValue < 97) || startValue > 122) {
+                            endValue = startValue;
+                        } else {
+                            endValue = startValue + ((key.charAt(pos)) - 65);
+                            if ((endValue > 90 && endValue < 97) || endValue > 122) {
+                                endValue -= 26;
+                            }
+                        }
+                        encryptedText += (char) endValue;
+                    }
+                }
+                System.out.println(encryptedText);
+                reader.close();
+            } else {
+                lbl_selectedFile.setText("Please select a file!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public void decrypt(ActionEvent actionEvent) {
     }
